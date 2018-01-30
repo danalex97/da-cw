d1f342ef1bb4caa31be9b2cff16109f521c8d0ce
ad5915 <dan.alex97@yahoo.com>
2018-01-30 18:23:58 +0000

    Fixed system1 local.

--- lib/peer1.ex
+++ lib/peer1.ex
@@ -1,26 +1,60 @@
   defmodule Peer1 do
-    def send_broadcast(self, peers, sent_messages) do
-      Enum.map(peers, fn (receiver) ->
-        IO.puts ["#{inspect self} SEND: ", inspect receiver]
+    def centralise(peers, recv_messages, sent_messages) do
+      results = Enum.reduce(peers, [], fn (peer, results) ->
+        sent = Map.get(sent_messages, peer, 0)
+        recv = Map.get(recv_messages, peer, 0)
+
+        results ++ [{sent, recv}]
+      end)
+
+      IO.puts ["#{inspect self()}:", inspect results]
+    end
+
+    def send_broadcast(self, peers, sent_messages, max_messages) do
+      receive do
+        :stop ->
+          send self, {:stop, sent_messages}
+          Process.exit(self(), :kill)
+      after 10 ->
+        :ok
+      end
+
+      if max_messages == 0 do
+        send self, {:stop, sent_messages}
+        Process.exit(self(), :kill)
+      end
+
+      sent_messages = Enum.reduce(peers, sent_messages, fn (receiver, sent_messages) ->
+        # IO.puts ["#{inspect self} SEND: ", inspect receiver]
         send receiver, {:peer_broadcast, self}
 
         msgs = Map.get(sent_messages, receiver, 0)
-        Map.put(sent_messages, receiver, msgs + 1)
+        sent_messages = Map.put(sent_messages, receiver, msgs + 1)
+
+        sent_messages
       end)
 
-      send_broadcast(self, peers, sent_messages)
+      send_broadcast(self, peers, sent_messages, max_messages - 1)
     end
 
-    def recv_broadcast(self, peers, recv_messages) do
+    def recv_broadcast(self, peers, recv_messages, timeout, send_process) do
       receive do
         {:peer_broadcast, sender} ->
-          IO.puts ["#{inspect self} RECV: ", inspect sender]
+          # IO.puts ["#{inspect self} RECV: ", inspect sender]
 
           msgs = Map.get(recv_messages, sender, 0)
-          Map.put(recv_messages, sender, msgs + 1)
-      end
+          recv_messages = Map.put(recv_messages, sender, msgs + 1)
 
-      recv_broadcast(self, peers, recv_messages)
+          recv_broadcast(self, peers, recv_messages, timeout, send_process)
+        {:timeout} ->
+          send send_process, :stop
+          receive do
+            {:stop, sent_messages} ->
+              centralise(peers, recv_messages, sent_messages)
+          end
+        {:stop, sent_messages} ->
+          centralise(peers, recv_messages, sent_messages)
+      end
     end
 
     defp loop do
@@ -33,12 +67,14 @@
           IO.puts ["#{inspect self()}: ", inspect peers]
 
           receive do
-            {:broadcast, _max_messages, _timeout} ->
+            {:broadcast, max_messages, timeout} ->
               sent_messages = %{}
               recv_messages = %{}
 
-              spawn(Peer1, :send_broadcast, [self(), peers, sent_messages])
-              recv_broadcast(self(), peers, recv_messages)
+              send_process = spawn(Peer1, :send_broadcast, [self(), peers, sent_messages, max_messages])
+              :timer.send_after(timeout, self(), {:timeout})
+
+              recv_broadcast(self(), peers, recv_messages, timeout, send_process)
           end
 
           loop()
