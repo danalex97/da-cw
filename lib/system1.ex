85066264e14561668b4b0c0aab56fed3df20e7f0
ad5915 <dan.alex97@yahoo.com>
2018-01-31 19:18:17 +0000

    Finished System1.

--- lib/system1.ex
+++ lib/system1.ex
@@ -7,9 +7,30 @@ defmodule System1 do
     loop()
   end
 
-  def start do
-    peers = Enum.to_list(for _ <- 0..(@n-1), do:
-      spawn(Peer1, :start, []))
+  defp local_spawn(_idx) do
+    spawn(Peer1, :start, [])
+  end
+
+  defp network_spawn(idx) do
+    Node.spawn(
+        :'peer#{idx}@peer#{idx}.localdomain',
+        Peer1,
+        :start,
+        [])
+  end
+
+  defp local_setup() do
+  end
+
+  defp network_setup() do
+    :timer.sleep(:timer.seconds(10))
+  end
+
+  defp start(spawn_function, setup) do
+    setup . ()
+
+    peers = Enum.to_list(for idx <- 0..(@n-1), do:
+      spawn_function . (idx + 1))
 
     Enum.map(Enum.zip(peers, 1..@n), fn ({peer, id}) ->
       send peer, {:bind, id, peers}
@@ -21,4 +42,13 @@ defmodule System1 do
 
     loop()
   end
+
+  def local_start() do
+    start(&local_spawn/1, &local_setup/0)
+  end
+
+  def network_start() do
+    start(&network_spawn/1, &network_setup/0)
+  end
+
 end
